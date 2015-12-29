e27624e54b72a09e6f2d408de974d3f02951deda
kutyel <flaviocorpa@gmail.com>
2015-12-29 22:52:07 +0100

    step 2: master/detail

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component} from 'angular2/core'
 
 interface Painter {
+    id: number;
     name: string;
     style: string;
     examples: string[];
@@ -10,20 +11,61 @@ interface Painter {
     selector: 'my-app',
     template: `
         <h1><img class="logoImg" src="img/brush.png" /> Famous painters</h1>
-        <h2>{{painter.name}}</h2>
-        <div><label>style: </label>{{painter.style}}</div>
-        <div><label>examples: </label>{{painter.examples}}</div>
-        <div>
-            <h3>Edit</h3>
-            <label>name: </label>
-            <div><input [(ngModel)]="painter.name" placeholder="name"></div>
+        <ul class="painters">
+            <li *ngFor="#p of painters"
+                [class.selected]="p === selectedPainter"
+                (click)="onSelect(p)">
+                <span class="badge">{{p.id}}</span> {{p.name}}
+            </li>
+        </ul>
+        <div *ngIf="selectedPainter">
+            <h2>{{selectedPainter.name}}</h2>
+            <div><label>style: </label>{{selectedPainter.style}}</div>
+            <div><label>examples: </label>{{selectedPainter.examples}}</div>
+            <div>
+                <h3>Edit</h3>
+                <label>name: </label>
+                <div><input [(ngModel)]="selectedPainter.name" placeholder="name"></div>
+            </div>
         </div>
     `
 })
-export class AppComponent {
-    public painter: Painter = {
+export class AppComponent {    
+    public painters = PAINTERS;    
+    public selectedPainter: Painter;
+    
+    onSelect(p: Painter) { this.selectedPainter = p; }
+}
+
+var PAINTERS: Painter[] = [
+    {
+        id: 1,
         name: 'Michelangelo',
         style: 'Renaissance',
         examples: ['David', 'Sistine Chapel', 'The Last Judgement']
-    };
-}
+    },
+    {
+        id: 2,
+        name: 'Raphael',
+        style: 'Renaissance',
+        examples: ['School at Athens', 'Lucretia', 'Saint George and the Dragon']
+    },
+    {
+        id: 3,
+        name: 'Vincent van Gogh',
+        style: 'Post-Impressionist',
+        examples: ['Self-portrait', 'The Starry Night', 'The Yellow House']
+    },
+    {
+        id: 4,
+        name: 'Claude Monet',
+        style: 'Impressionist',
+        examples: ['Morning on the Seine', 'The Woman in the Green Dress', 'Sunrise']
+    },
+    {
+        id: 5,
+        name: 'Johannes Vermeer',
+        style: 'Baroque',
+        examples: ['The Milkmaid', 'Girl with a Pearl Earring', 'The Music Lesson']
+    }
+];
