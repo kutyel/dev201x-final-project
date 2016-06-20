0383fc1eacad2d2fbde366c7e63f57ead99178aa
kutyel <flaviocorpa@gmail.com>
2016-06-20 22:19:36 +0200

    back to the mock!

---
+++ app/painters.mock.ts
@@ -0,0 +1,34 @@
+import { Painter } from './painter';
+
+export var PAINTERS: Painter[] = [
+    {
+        id: 1,
+        name: 'Michelangelo',
+        style: 'Renaissance',
+        examples: ['David', 'Sistine Chapel', 'The Last Judgement']
+    },
+    {
+        id: 2,
+        name: 'Raphael',
+        style: 'Renaissance',
+        examples: ['School at Athens', 'Lucretia', 'Saint George and the Dragon']
+    },
+    {
+        id: 3,
+        name: 'Vincent van Gogh',
+        style: 'Post-Impressionist',
+        examples: ['Self-portrait', 'The Starry Night', 'The Yellow House']
+    },
+    {
+        id: 4,
+        name: 'Claude Monet',
+        style: 'Impressionist',
+        examples: ['Morning on the Seine', 'The Woman in the Green Dress', 'Sunrise']
+    },
+    {
+        id: 5,
+        name: 'Johannes Vermeer',
+        style: 'Baroque',
+        examples: ['The Milkmaid', 'Girl with a Pearl Earring', 'The Music Lesson']
+    }
+];
